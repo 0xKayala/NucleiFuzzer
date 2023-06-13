4a23af1870973ec546fee0942d7e451996fb5b36
Kayala Satya Prakash <satyaprakash99915@gmail.com>
2023-06-13 20:59:24 +0530

    Update NucleiFuzzer.sh

--- NucleiFuzzer.sh
+++ NucleiFuzzer.sh
@@ -12,11 +12,11 @@ echo -e "\e[91m
 
 # Help menu
 display_help() {
-    echo -e "NucleiFuzzer is a powerful automation tool for detecting OpenRedirect vulnerabilities in web applications\n\n"
+    echo -e "NucleiFuzzer is a powerful automation tool for detecting xss,sqli,ssrf,open-redirect..etc vulnerabilities in web applications\n\n"
     echo -e "Usage: $0 [options]\n\n"
     echo "Options:"
     echo "  -h, --help              Display help information"
-    echo "  -d, --domain <domain>   Domain to scan for open redirects"
+    echo "  -d, --domain <domain>   Domain to scan for xss,sqli,ssrf,open-redirect..etc vulnerabilities"
     exit 0
 }
 
@@ -48,10 +48,10 @@ fi
 
 # Step 3: Get the vulnerable parameters of the given domain name using ParamSpider tool and save the output into a text file
 echo "Running ParamSpider on $domain"
-python3 /home/kali/ParamSpider/paramspider.py -d "$domain" -o /home/kali/OpenRedirector/paramspider_output.txt
+python3 /home/kali/ParamSpider/paramspider.py -d "$domain" --exclude png,jpg,gif,jpeg,swf,woff,gif,svg --quiet -o /home/kali/paramspider_output.txt
 
 # Check if ParamSpider found any unique URLs
-unique_urls=$(grep -oP '(?<=\=)[^&]+' /home/kali/Desktop/Bugs/OpenRedirect/paramspider_output.txt | sort -u | wc -l)
+unique_urls=$(grep -oP '(?<=\=)[^&]+' /home/kali/paramspider_output.txt | sort -u | wc -l)
 if [ $unique_urls -eq 0 ]; then
     echo "No URLs Found"
     exit 1
@@ -59,7 +59,7 @@ fi
 
 # Step 4: Run the nuclei fuzzer tool on the above text file
 echo "Running nuclei fuzzer on paramspider_output.txt"
-nuclei -l /home/kali/Desktop/Bugs/OpenRedirect/paramspider_output.txt -t fuzzing-templates
+nuclei -l /home/kali/paramspider_output.txt -t fuzzing-templates -rl 05
 
 # Step 5: End with general message as the scan is completed
 echo "Scan is completed."
