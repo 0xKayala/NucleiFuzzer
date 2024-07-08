e38df58f36b81f8f91774454cd9eae4945fe5661
Satya Prakash <satyaprakash99915@gmail.com>
2024-07-08 13:23:57 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 
-# Rename and move NucleiFuzzer.sh file to /usr/local/bin/nucleifuzzer
+# Rename and move NucleiFuzzer.sh file to /usr/bin/nf
 sudo mv NucleiFuzzer.sh /usr/bin/nf
 
 # Make the NucleiFuzzer file executable
 sudo chmod u+x /usr/bin/nf
-
-# Remove the NucleiFuzzer folder from the home directory
-if [ -d "$home_dir/NucleiFuzzer" ]; then
-    echo "Removing NucleiFuzzer folder..."
-    rm -r "$home_dir/NucleiFuzzer"
-fi
 
 echo "NucleiFuzzer has been installed successfully! Now Enter the command 'nf' to run the tool."
