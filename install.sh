5891cb6a67707a2fbd521187e5a03a418a4f6f3a
evex80 <137130616+evex80@users.noreply.github.com>
2024-08-30 04:51:39 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Rename and move NucleiFuzzer.sh file to /usr/bin/nf
-sudo mv NucleiFuzzer.sh /usr/bin/nf
+sudo cp NucleiFuzzer.sh /usr/bin/nf
 
 # Make the NucleiFuzzer file executable
 sudo chmod u+x /usr/bin/nf
