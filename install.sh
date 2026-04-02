c28eb01f1c9233ac9150b8e490f9d38ec1450a4e
Satya Prakash <satyaprakash99915@gmail.com>
2026-04-02 16:49:26 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Rename and move NucleiFuzzer.sh file to /usr/bin/nf
-sudo cp nf.sh /usr/bin/nf
+sudo cp nucleifuzzer.sh /usr/bin/nf
 
 # Make the NucleiFuzzer file executable
 sudo chmod +x /usr/bin/nf
