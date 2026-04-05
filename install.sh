23a96cd686b95368b7beafe133b8199ea49a9899
Satya Prakash <satyaprakash99915@gmail.com>
2026-04-05 23:13:44 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,15 +5,14 @@ INSTALL_DIR="/opt/nucleifuzzer"
 echo "[*] Installing NucleiFuzzer..."
 
 sudo mkdir -p $INSTALL_DIR
-
-sudo cp -r core config $INSTALL_DIR
-sudo cp nucleifuzzer.sh $INSTALL_DIR
+sudo cp -r ./* $INSTALL_DIR
 
 sudo ln -sf $INSTALL_DIR/nucleifuzzer.sh /usr/bin/nf
 sudo chmod +x /usr/bin/nf
 
+echo ""
 echo "======================================"
 echo "✅ Installed Successfully"
 echo "📁 Location: $INSTALL_DIR"
-echo "⚡ Run: nf -d example.com --ai"
+echo "⚡ Run: nf -h"
 echo "======================================"
